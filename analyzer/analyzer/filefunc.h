7c4f70508c08fba3a32b5b2bef05ff8123d49673
Jian HanBin <jhanbin@lscm.ad>
2016-10-26 16:32:32 +0800

    20140922 all comment clean

--- analyzer/analyzer/filefunc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <afxcmn.h>
-#include <vector>
-
-CString ChooseFolderDlg();
-BOOL ReadFileCustom(CObject * co, size_t n, CString fp);
-BOOL WriteFileCustom(CObject * co, size_t n, CString fp);
-void LoadFileList(const CString &m_filePath, std::vector<CString> &filelist);
--- analyzer/analyzer/funT/nrerror.h
+++
@@ -1,8 +0,0 @@
-#ifndef NRERROR_H
-#define NRERROR_H
-
-
-void nrerror(char error_text[]);
-
-
-#endif
